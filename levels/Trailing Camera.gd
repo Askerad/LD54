74a5cd84452672e127ff12d12e0d5c4b11973e47
Neon Kuroasa <neon.kuroasa@gmail.com>
2023-09-30 14:50:08 +0200

    Setup Camera

--- levels/Trailing Camera.gd
+++ levels/Trailing Camera.gd
@@ -1,10 +1,10 @@
 extends Camera2D
 
-@export var target: Node2D;
+#@export var target: Node2D;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	position.y = lerp(position.y ,target.position.y, 0.1) 
+#	position.y = lerp(position.y ,target.position.y, 0.1) 
 	
 	pass
