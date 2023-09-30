896dadd9c75c9740eb0086ed5732cf35fd065f4a
Neon Kuroasa <neon.kuroasa@gmail.com>
2023-09-30 14:41:47 +0200

    Infinite walls !

--- levels/WallManager.gd
+++ levels/WallManager.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 @export var espacement: float = 1000;
 @export var speed: float = 50;
 
@@ -9,7 +10,7 @@ func _process(delta):
 	
 	if espacement < 0 : espacement = 0;
 	
-	$"Wall Left".position.x = -espacement/2
-	$"Wall Right".position.x = espacement/2
+	$"ParallaxBackground/ParallaxLayer/Wall Left".position.x = -espacement/2
+	$"ParallaxBackground/ParallaxLayer/Wall Right".position.x = espacement/2
 	
 	pass
