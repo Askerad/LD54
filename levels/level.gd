d1cb36bbbf240ed136137a19d81c7a64bf5eb635
Neon Kuroasa <neon.kuroasa@gmail.com>
2023-10-01 23:22:08 +0200

    MUSIQUE

--- levels/level.gd
+++ levels/level.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var fondnoir = $CharacterBody2D/Camera2D/UiCanvas/Fondnoir
 
 @onready var falling_sfx = $FallingSFX
+@onready var music = $AudioStreamPlayer
+@onready var game_over_drone = $GameOverDrone
 
 
 func _on_restart_button_pressed():
@@ -13,9 +15,9 @@ func _on_restart_button_pressed():
 func _on_played_died():
 	fondnoir.visible = true;
 	game_over_screen.visible = true;
+	music.stop()
+	game_over_drone.play()
 	
-
-
 func _on_kill_box_body_entered(body):
 	if body is CharacterBody2D:
 		falling_sfx.play()
